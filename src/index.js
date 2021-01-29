ff349316efaad2d5acc95e239d242998dd21f737
kherma <kamil.herma01@gmail.com>
2021-01-29 16:15:36 +0100

    Add basic book component structure

--- src/index.js
+++ src/index.js
@@ -12,9 +12,22 @@ const Booklist = () => {
 const Book = () => {
   return (
     <article>
-      <p>This is a book.</p>
+      <Image />
+      <Title />
+      <Author />
     </article>
   );
 };
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81FeR-7X-WL._AC_UL200_SR200,200_.jpg"
+    alt="book 1"
+  />
+);
+
+const Title = () => <h2>Just as I Am: A Memoir</h2>;
+
+const Author = () => <p>by Cicely Tyson</p>;
 
 ReactDom.render(<Booklist />, document.getElementById('root'));
