2d9a18cbd58f54c40cec35c5c580f952214b1e7a
kherma <kamil.herma01@gmail.com>
2021-01-29 19:19:49 +0100

    Render books from pros

--- src/index.js
+++ src/index.js
@@ -3,50 +3,53 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 // Variables
-const firstBook = {
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/81FeR-7X-WL._AC_UL200_SR200,200_.jpg',
-  title: 'Just as I Am: A Memoir',
-  author: 'Cicely Tyson',
-};
-
-const secondBook = {
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/81YzHKeWq7L._AC_UL200_SR200,200_.jpg',
-  title: 'The Midnight Library: A Novel',
-  author: 'Matt Haig',
-};
+const books = [
+  {
+    id: '1',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/81FeR-7X-WL._AC_UL200_SR200,200_.jpg',
+    title: 'Just as I Am: A Memoir',
+    author: 'Cicely Tyson',
+  },
+  {
+    id: '2',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/81YzHKeWq7L._AC_UL200_SR200,200_.jpg',
+    title: 'The Midnight Library: A Novel',
+    author: 'Matt Haig',
+  },
+  {
+    id: '3',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/81ICvbFe2%2BL._AC_UL200_SR200,200_.jpg',
+    title: 'The Vanishing Half: A Novel',
+    author: 'Brit Bennett',
+  },
+];
 
 const Booklist = () => {
   return (
     <section className="bookList">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-          velit sunt laboriosam a veniam officia reiciendis modi officiis magnam
-          iste!
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book, index) => (
+        <Book key={book.id} {...book} counter={index + 1} />
+      ))}
     </section>
   );
 };
 
-const Book = ({ img, title, author, children }) => {
+const Book = ({ img, title, author, counter }) => {
+  const clickHandler = () => {
+    console.log('Hello world');
+  };
   return (
     <article className="book">
       <img src={img} alt="book" />
       <h2>{title}</h2>
       <h4>{author.toUpperCase()}</h4>
-      {children}
+      <small className="counter">#{counter}</small>
+      <button type="button" onClick={clickHandler}>
+        Button
+      </button>
     </article>
   );
 };
