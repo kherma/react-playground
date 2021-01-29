acbb31168d08ebbe79f2ab48b42ddd39e13637dd
kherma <kamil.herma01@gmail.com>
2021-01-29 14:54:20 +0100

    Create basic component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Greeting = () => {
+  return <h1>Hello World</h1>;
+};
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
