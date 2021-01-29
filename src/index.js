ee042f252de00c97abbcea2c64a8c9ff83edd623
kherma <kamil.herma01@gmail.com>
2021-01-29 15:03:48 +0100

    Add .eslintcache to gitignore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// const Greeting = () => {
+//   return <h1>Hello World</h1>;
+// };
+
 const Greeting = () => {
-  return <h1>Hello World</h1>;
+  return React.createElement('h1', {}, 'Hello World');
 };
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
