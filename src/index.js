f4585eae7d2a2a2a8329548208eb56f73749b160
kherma <kamil.herma01@gmail.com>
2021-01-29 17:13:51 +0100

    Cleanup code

--- src/index.js
+++ src/index.js
@@ -6,36 +6,23 @@ const Booklist = () => {
   return (
     <section className="bookList">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 };
 
 const Book = () => {
+  const title = 'Just as I Am: A Memoir';
+  const author = 'Cicely Tyson';
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81FeR-7X-WL._AC_UL200_SR200,200_.jpg"
+        alt="book 1"
+      />
+      <h2>{title}</h2>
+      <p>{author.toUpperCase()}</p>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81FeR-7X-WL._AC_UL200_SR200,200_.jpg"
-    alt="book 1"
-  />
-);
-
-const Title = () => <h2>Just as I Am: A Memoir</h2>;
-
-const Author = () => (
-  <p style={{ color: '#617d98', fontSize: '0.75rem' }}>by Cicely Tyson</p>
-);
 
 ReactDom.render(<Booklist />, document.getElementById('root'));
