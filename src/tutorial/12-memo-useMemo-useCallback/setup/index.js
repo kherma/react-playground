8a7db704198bd2951fa6d9176259818d6381078e
kherma <kamil.herma01@gmail.com>
2021-02-07 14:14:01 +0100

    Learn useMemo, React.memo, useCallback

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,48 +1,62 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import React, { useState, useEffect } from 'react';
+import { useFetch } from '../../9-custom-hooks/setup/2-useFetch';
 
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products'
+const url = 'https://course-api.com/javascript-store-products';
 
 // every time props or state changes, component re-renders
+const calculateMostExpensive = (data) => {
+  console.log('hello');
+  return (
+    data.reduce((total, item) => {
+      const price = item.fields.price;
+      if (price >= total) total = price;
+      return total;
+    }, 0) / 100
+  );
+};
 
 const Index = () => {
-  const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Count : {count}</h1>
-      <button className='btn' onClick={() => setCount(count + 1)}>
+      <button className="btn" onClick={() => setCount(count + 1)}>
         click me
       </button>
+      <h2 style={{ marginTop: '3rem' }}>
+        Most Expensive : ${calculateMostExpensive(products)}
+      </h2>
       <BigList products={products} />
     </>
-  )
-}
+  );
+};
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products }) => {
+  useEffect(() => {
+    console.log('Call');
+  });
   return (
-    <section className='products'>
+    <section className="products">
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product}></SingleProduct>;
       })}
     </section>
-  )
-}
+  );
+});
 
 const SingleProduct = ({ fields }) => {
-  let { name, price } = fields
-  price = price / 100
-  const image = fields.image[0].url
+  let { name, price } = fields;
+  price = price / 100;
+  const image = fields.image[0].url;
 
   return (
-    <article className='product'>
+    <article className="product">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
     </article>
-  )
-}
-export default Index
+  );
+};
+export default Index;
