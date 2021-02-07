256e7493ae21e920732729b9a2375d295676b183
kherma <kamil.herma01@gmail.com>
2021-02-07 09:26:27 +0100

    Learn custom hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/8-useContext/setup/3-context-api';
+import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example';
 
 function App() {
   return (
